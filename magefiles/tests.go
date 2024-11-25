72b8adc2ecf68f676fe3d8f0e7682b3633154354
fabiovincenzi <93596376+fabiovincenzi@users.noreply.github.com>
2024-11-25 14:46:38 +0100

    Update tests.go
    Signed-off-by: fabiovincenzi <93596376+fabiovincenzi@users.noreply.github.com>

--- magefiles/tests.go
+++ magefiles/tests.go
@@ -1,59 +1,75 @@
-name: Test
+//go:build mage
 
-on:
-  workflow_call:
+//nolint:wrapcheck
+package main
 
-permissions:
-  contents: read
+import (
+	"os"
 
-jobs:
-  go:
-    name: Test Go
-    strategy:
-      matrix:
-        runner: [macos-latest, ubuntu-latest, windows-latest]
-    runs-on: ${{ matrix.runner }}
-    steps:
-      - uses: actions/checkout@v4
-      - name: Setup Go
-        uses: actions/setup-go@v5
-        with:
-          go-version: "1.22"
-          check-latest: true
-          cache: false
-      - name: Install mage
-        run: go install github.com/magefile/mage@v1.15.0
-      - name: Run unit tests
-        run: mage test:unit
+	"github.com/magefile/mage/mg"
+	"github.com/magefile/mage/sh"
+)
 
-  python:
-    name: Test Python
-    strategy:
-      matrix:
-        runner: [macos-latest, ubuntu-latest, windows-latest]
-        python: ["3.8", "3.9", "3.10", "3.11", "3.12"]
-    runs-on: ${{ matrix.runner }}
-    steps:
-      - uses: actions/checkout@v4
-      - name: Setup Python
-        uses: actions/setup-python@v5
-        with:
-          python-version: ${{ matrix.python }}
-      - name: Setup Go
-        uses: actions/setup-go@v5
-        with:
-          go-version: "1.22"
-          check-latest: true
-          cache: false
-      - name: Install mage
-        run: go install github.com/magefile/mage@v1.15.0
-      - name: Install our package in editable mode
-        run: pip install -e .
-      - name: Initialize MLflow repo
-        run: mage repo:init
-      - name: Install dependencies
-        run: pip install pytest==8.1.1 psycopg2-binary==2.9.9 -e .mlflow.repo
-      - name: Run integration tests
-        run: mage test:python
-        # Temporary workaround for failing tests
-        continue-on-error: ${{ matrix.runner != 'ubuntu-latest' }}
+type Test mg.Namespace
+
+func cleanUpMemoryFile() error {
+	// Clean up :memory: file
+	filename := ":memory:"
+	_, err := os.Stat(filename)
+
+	if err == nil {
+		// File exists, delete it
+		err = os.Remove(filename)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// Run mlflow Python tests against the Go backend.
+func (Test) Python() error {
+	libpath, err := os.MkdirTemp("", "")
+	if err != nil {
+		return err
+	}
+
+	// Remove the Go binary
+	defer os.RemoveAll(libpath)
+	//nolint:errcheck
+	defer cleanUpMemoryFile()
+
+	// Build the Go binary in a temporary directory
+	if err := sh.RunV("python", "-m", "mlflow_go.lib", ".", libpath); err != nil {
+		return nil
+	}
+
+	//  Run the tests (currently just the server ones)
+	if err := sh.RunWithV(map[string]string{
+		"MLFLOW_GO_LIBRARY_PATH": libpath,
+	}, "pytest",
+		"--confcutdir=.",
+		".mlflow.repo/tests/tracking/test_rest_tracking.py",
+		".mlflow.repo/tests/tracking/test_model_registry.py",
+		".mlflow.repo/tests/store/tracking/test_sqlalchemy_store.py",
+		".mlflow.repo/tests/store/model_registry/test_sqlalchemy_store.py",
+		"-k",
+		"not [file",
+		// "-vv",
+	); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Run the Go unit tests.
+func (Test) Unit() error {
+	return sh.RunV("go", "test", "./pkg/...")
+}
+
+// Run all tests.
+func (Test) All() {
+	mg.Deps(Test.Unit, Test.Python)
+}
