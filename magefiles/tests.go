9ce952caccb4e2f0f87c95b8c57529f35c70d8fb
fabiovincenzi <93596376+fabiovincenzi@users.noreply.github.com>
2024-11-26 16:34:59 +0100

    Update tests.go
    Signed-off-by: fabiovincenzi <93596376+fabiovincenzi@users.noreply.github.com>

--- magefiles/tests.go
+++ magefiles/tests.go
@@ -91,7 +91,10 @@ func (Test) Python() error {
 	}
 	if err := sh.RunWithV(testEnv, "python", "-m", "pytest",
 		"--confcutdir=.",
+		".mlflow.repo/tests/tracking/test_rest_tracking.py",
 		".mlflow.repo/tests/tracking/test_model_registry.py",
+		".mlflow.repo/tests/store/tracking/test_sqlalchemy_store.py",
+		".mlflow.repo/tests/store/model_registry/test_sqlalchemy_store.py",
 		"-k", "not [file",
 	); err != nil {
 		return fmt.Errorf("Python tests failed: %w", err)
