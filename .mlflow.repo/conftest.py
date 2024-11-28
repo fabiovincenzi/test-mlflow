d6bdca00c63d92ed9a9f34a72f191fde55f057c0
Fabio Vincenzi <fabio.vincenzi2001@gmail.com>
2024-11-28 13:56:17 +0000

    test

--- .mlflow.repo/conftest.py
+++ .mlflow.repo/conftest.py
@@ -1,10 +1,6 @@
 import json
-import logging
 import os
 import posixpath
-import re
-import shutil
-import subprocess
 import sys
 import threading
 import traceback
@@ -12,10 +8,7 @@ import traceback
 import click
 import pytest
 from mlflow.environment_variables import _MLFLOW_TESTING, MLFLOW_TRACKING_URI
-from mlflow.utils.os import is_windows
 from mlflow.version import VERSION
-
-from tests.helper_functions import get_safe_port
 
 
 def pytest_addoption(parser):
@@ -297,113 +290,8 @@ def pytest_terminal_summary(terminalreporter, exitstatus, config):
                 terminalreporter.write(f"{idx}: {child}\n")
 
 
-logger = logging.getLogger(__name__)
-
-
-@pytest.fixture(scope="module", autouse=True)
-def clean_up_envs():
-    """
-    Clean up virtualenvs and conda environments created during tests to save disk space.
-    """
-    yield
-
-    if "GITHUB_ACTIONS" in os.environ:
-        logger.info("Cleaning up virtual environments...")
-        try:
-            from mlflow.utils.virtualenv import _get_mlflow_virtualenv_root
-
-            mlflow_virtualenv_root = _get_mlflow_virtualenv_root()
-            logger.info(f"Removing virtualenv root: {mlflow_virtualenv_root}")
-            shutil.rmtree(mlflow_virtualenv_root, ignore_errors=True)
-        except Exception as e:
-            logger.error(f"Error while removing virtualenv root: {e}")
-
-        if not is_windows():
-            try:
-                conda_info = json.loads(
-                    subprocess.check_output(["conda", "info", "--json"], text=True)
-                )
-                logger.info(f"Condas info: {conda_info}")
-                root_prefix = conda_info["root_prefix"]
-                regex = re.compile(r"mlflow-\w{32,}")
-                for env in conda_info["envs"]:
-                    if env == root_prefix:
-                        continue
-                    if regex.fullmatch(os.path.basename(env)):
-                        logger.info(f"Removing Conda environment: {env}")
-                        shutil.rmtree(env, ignore_errors=True)
-            except subprocess.CalledProcessError as e:
-                logger.error(f"Error while getting Conda info: {e}")
-            except Exception as e:
-                logger.error(f"Error while cleaning Conda environments: {e}")
-
-
 @pytest.fixture(scope="session", autouse=True)
 def enable_mlflow_testing():
     with pytest.MonkeyPatch.context() as mp:
         mp.setenv(_MLFLOW_TESTING.name, "TRUE")
         yield
-
-
-@pytest.fixture(scope="session", autouse=True)
-def serve_wheel(request, tmp_path_factory):
-    """
-    Models logged during tests have a dependency on the dev version of MLflow built from
-    source (e.g., mlflow==1.20.0.dev0) and cannot be served because the dev version is not
-    available on PyPI. This fixture serves a wheel for the dev version from a temporary
-    PyPI repository running on localhost and appends the repository URL to the
-    `PIP_EXTRA_INDEX_URL` environment variable to make the wheel available to pip.
-    """
-    if not request.config.getoption("--serve-wheel"):
-        yield  # pytest expects a generator fixture to yield
-        return
-
-    root = tmp_path_factory.mktemp("root")
-    mlflow_dir = root.joinpath("mlflow")
-    mlflow_dir.mkdir()
-    port = get_safe_port()
-    try:
-        repo_root = subprocess.check_output(
-            [
-                "git",
-                "rev-parse",
-                "--show-toplevel",
-            ],
-            text=True,
-        ).strip()
-    except subprocess.CalledProcessError:
-        # Some tests run in a Docker container where git is not installed.
-        # In this case, assume we're in the root of the repo.
-        repo_root = "."
-
-    subprocess.run(
-        [
-            sys.executable,
-            "-m",
-            "pip",
-            "wheel",
-            "--wheel-dir",
-            mlflow_dir,
-            "--no-deps",
-            repo_root,
-        ],
-        check=True,
-    )
-    with subprocess.Popen(
-        [
-            sys.executable,
-            "-m",
-            "http.server",
-            str(port),
-        ],
-        cwd=root,
-    ) as prc:
-        try:
-            url = f"http://localhost:{port}"
-            if existing_url := os.environ.get("PIP_EXTRA_INDEX_URL"):
-                url = f"{existing_url} {url}"
-            os.environ["PIP_EXTRA_INDEX_URL"] = url
-
-            yield
-        finally:
-            prc.terminate()
