25fe4d44edb14d20905279b554fc0b31e1190cc1
fabiovincenzi <93596376+fabiovincenzi@users.noreply.github.com>
2024-12-12 09:50:32 +0100

    Update override_test_sqlalchemy_store.py

--- tests/override_test_sqlalchemy_store.py
+++ tests/override_test_sqlalchemy_store.py
@@ -2,6 +2,12 @@ import pytest
 from mlflow.exceptions import MlflowException
 from mlflow.store.tracking.sqlalchemy_store import SqlAlchemyStore
 
+def test_search_experiments_filter_by_time_attribute(store: SqlAlchemyStore):
+     ()
+
+
+ def test_search_experiments_order_by_time_attribute(store: SqlAlchemyStore):
+     ()
 
 def test_log_batch_internal_error(store: SqlAlchemyStore):
     ()
